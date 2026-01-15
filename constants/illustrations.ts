e0e3374103eba62dda442ea1a60ef8bdb45c5edf
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2026-01-15 16:47:45 +0530

    feat(ui):  --BASE STYLE CONFIG-- add typography, color themes, and component styles
    - Define custom properties for typography and color palette in globals.css
    - Add base styles for headings and interactive elements
    - Introduce reusable component classes for buttons and inputs
    - Include react-icons dependency and create illustration constants file
    - Update package.json to reflect new dependency installation

---
+++ constants/illustrations.ts
@@ -0,0 +1,62 @@
+import {
+  FaLeaf as Grass,
+  FaRegCircle as Normal,
+  FaRegSnowflake as Ice,
+  FaGhost as Ghost,
+  FaCat as Pokemon2,
+  FaCircle as Weight,
+} from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+
+import {
+  GiPoisonBottle as Poison,
+  GiLibertyWing as Flying,
+  GiSeaDragon as Dragon,
+  GiPunch as Fighting,
+  GiFairyWings as Fairy,
+  GiFallingRocks as Rock,
+  GiPowerLightning as Electric,
+  GiPsychicWaves as Psychic,
+  GiRabbit as Pikachu,
+  GiSparkSpirit as Pokemon3,
+} from "react-icons/gi";
+import { RiArrowLeftLine as ArrowLeft } from "react-icons/ri";
+import { ImFire as Fire } from "react-icons/im";
+import { IoIosBug as Bug } from "react-icons/io";
+import {
+  IoChevronDown as ArrowDown,
+  IoFilterOutline as Filter,
+} from "react-icons/io5";
+import { BsDropletFill as Water, BsNutFill as Steel } from "react-icons/bs";
+import { WiMoonAltWaxingCrescent3 as Dark } from "react-icons/wi";
+import { MdTerrain as Ground } from "react-icons/md";
+import { IconType } from "react-icons/lib";
+
+export const Icons: IconType[] = [
+  Grass,
+  Fire,
+  Water,
+  Bug,
+  Normal,
+  Poison,
+  Electric,
+  Ground,
+  Fairy,
+  Fighting,
+  Psychic,
+  Rock,
+  Ghost,
+  Ice,
+  Dragon,
+  Dark,
+  Steel,
+  Pokemon2,
+  Pokemon3,
+  Pikachu,
+  Flying,
+  ArrowLeft,
+  ArrowDown,
+  Filter,
+  FiSearch,
+  Weight,
+];
